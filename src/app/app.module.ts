81b022788868947d1ce2e92e13360e488e5f15f5
MaxiTRR <maxihtorres@gmail.com>
2022-07-19 18:22:47 -0300

    Cambios generales en el estilo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,9 +10,7 @@ import { InfoComponent } from './components/info/info.component';
 import { ImageProfileComponent } from './components/image-profile/image-profile.component';
 import { InfoTextComponent } from './components/info-text/info-text.component';
 import { ExperienceComponent } from './components/experience/experience.component';
-import { ExpInfoComponent } from './components/exp-info/exp-info.component';
 import { EducationComponent } from './components/education/education.component';
-import { EduCardComponent } from './components/edu-card/edu-card.component';
 import { SkillsComponent } from './components/skills/skills.component';
 import { ProjectsComponent } from './components/projects/projects.component';
 import { FooterComponent } from './components/footer/footer.component';
@@ -26,9 +24,7 @@ import { FooterComponent } from './components/footer/footer.component';
     ImageProfileComponent,
     InfoTextComponent,
     ExperienceComponent,
-    ExpInfoComponent,
     EducationComponent,
-    EduCardComponent,
     SkillsComponent,
     ProjectsComponent,
     FooterComponent
--- src/app/components/education/education.component.ts
+++ src/app/components/education/education.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { faPencil} from '@fortawesome/free-solid-svg-icons';
+import { faXmark} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-education',
@@ -6,6 +8,9 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./education.component.css']
 })
 export class EducationComponent implements OnInit {
+  faPencil=faPencil;
+  faXmark=faXmark;
+  
   @Input() childMessage:boolean = false;
   
 
