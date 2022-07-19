5700ad57b2662ab584c697c59107db768d9be9a8
MaxiTRR <maxihtorres@gmail.com>
2022-07-19 18:47:58 -0300

    Creacion del file models.model.ts, con todos los models en un file

---
+++ src/app/models/models.model.ts
@@ -0,0 +1,54 @@
+export class Contacto{
+    id:number = 0;
+    email:string = '';
+    linkInstagram:string = "https://www.instagram.com/";
+    linkLinkedin:string = "https://ar.linkedin.com/";
+    linkFacebook:string = "https://www.facebook.com/";
+    linkGithub:string = "https://github.com/"
+}
+
+export class Cuenta{
+    id:number = 0;
+    email:string = '';
+    userName:string = '';
+    password:string = '';
+}
+
+export class Direccion{
+    id:number = 0;
+    pais:string = '';
+    provincia:string = '';
+    localidad:string = '';
+    domicilio:string = '';
+    codPostal:number = 0;
+    
+}
+
+export class Educacion{
+    id:number = 0;
+    nombreInst:string = '';
+    periodo:string = '';
+    titulo:string = '';
+    //certificado:string = '';
+    //logo:string = '';
+}
+
+export class Exp{
+    id:number = 0;
+    lugar:string = '';
+    periodo:string = '';
+    area:string = '';
+    rol:string = '';
+    //logo:string = '';
+}
+
+export class InfoPersoanl{
+    id:number = 0;
+    titulo:string ='';
+    descripcion:string='';
+    //imgProfile:string ='';
+}
+
+export class Proyectos{
+    id:number = 0;
+}
